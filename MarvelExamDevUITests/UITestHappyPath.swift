4e7a77ead2f5dcca6eff98492d751532a05a68a1
genaro <lgarvizuv@srpago.com>
2021-09-11 13:13:31 -0500

    Fastlane integration

--- MarvelExamDevUITests/UITestHappyPath.swift
+++ MarvelExamDevUITests/UITestHappyPath.swift
@@ -35,13 +35,4 @@ class UITestHappyPath: XCTestCase {
         }
 
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
